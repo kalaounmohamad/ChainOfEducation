fbdd530d73619789ef9e4687ddde60466f230dc2
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-05-28 17:11:18 +0300

    2

--- src/Components/chemistry.js
+++ src/Components/chemistry.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Chemistry() {
-  return <h1>In chemistry now</h1>;
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/JdnQyfzg1sg"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  );
 }
